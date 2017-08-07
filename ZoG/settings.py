fe23fa2936958af7a127dade70aa7946624ff5a6
WaltonSimons <masteroskar@gmail.com>
2017-08-07 20:29:26 +0200

    Update settings.py

--- ZoG/settings.py
+++ ZoG/settings.py
@@ -76,11 +76,10 @@ WSGI_APPLICATION = 'ZoG.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {}
-
 db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
-
+DATABASES = {
+    'default': db_from_env
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
